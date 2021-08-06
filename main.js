bf759277c3db092276fafde11a5721539467ed52
Klaudia Osowska <klaudia.osowska21@gmail.com>
2021-08-06 17:54:24 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName= 'Klaudia';
+const age= 28;
+
+console.log (firstName);
+console.log (age);
+
+alert('Kocham Cię Macio!')
+
+console.log (`Nazywam się ${firstName} i mam ${age} lat`)
